16c90e2094cf2ac50d6193d7fad9c504010ca1be
Vainoras Sakavičius <vainorassakavicius@Vainorass-MacBook-Air.local>
2025-05-07 18:52:42 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+let cards = [];
+let flippedCards = [];
+let matchedPairs = 0;
+let isPlaying = false;
+let timer;
+let seconds = 0;
+let tries = 0;
+
+const gameBoard = document.getElementById('game-board');
+const startButton = document.getElementById('start-button');
+const resetButton = document.getElementById('reset-button');
+const timeDisplay = document.getElementById('time');
+const triesDisplay = document.getElementById('tries');
+const bestTimeDisplay = document.getElementById('bestTime');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+ 
